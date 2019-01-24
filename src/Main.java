1846cd73c44f825e2121b09fce471ad8679b51ee
Eugenio Mattei <emattei.phd@gmail.com>
2019-01-24 16:20:34 -0500

    - First two sections

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("Hello, World!");
+        /* Integer type
+        // 64 bit : -2^63 <= x <= 2^63 -1
+        long myLongValue = 64L;
+        // 32 bit : -2_147_483_648 <= x <= 2_147_483_647
+        int  myIntValue = 32;
+        // 16 bit : -32768 <= x <= 32767
+        short myShortValue = 16;
+        // 8 bit : -128 <= x <= 127
+        byte myByteValue =  8;
+
+        //Double precision type
+        float myFloatValue = 32f;
+        // 64 bit : 16 digits precision
+        // faster on modern computers
+        // built-in functions (sin, cos) use double by default
+        double myDoubleValue = 64d;
+        // 16 bit
+        char myChar = '\u00A9';
+        boolean isName = true;
+        String myString = "Hello";
+        boolean wasCar = isCar ? true:false;*/
+
+
     }
+
 }
