20abbabee0f69ef9e5bb1a46518eb4444217db65
Eugenio Mattei <emattei.phd@gmail.com>
2019-01-30 21:02:56 -0500

    - section4

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -24,8 +26,79 @@ public class Main {
         boolean isName = true;
         String myString = "Hello";
         boolean wasCar = isCar ? true:false;*/
-
+        int score = calculateScore("Tim",500);
+        calculateScore(500);
+        calculateScore();
 
     }
+
+    public static int calculateScore(String playerName, int score){
+        System.out.println("Player "+ playerName + " scored " + score + " points");
+        return score*1000;
+    }
+
+    public static int calculateScore(int score){
+        System.out.println("Unnamed player scored " + score + " points");
+        return score*1000;
+    }
+
+    public static void calculateScore(){
+        System.out.println("No player name, no player score");
+    }
+
+    public static boolean areEqualByThreeDecimalPlaces(double a, double b){
+        if(a-b<0.001){
+            return true;
+
+        }else{
+            return false;
+        }
+
+    }
+    public static boolean bark(boolean barking, int hourOfDay){
+        boolean wakeUp = false;
+
+        if(barking){
+            System.out.println("Here");
+            if(hourOfDay < 8 || hourOfDay > 22){
+                System.out.println("Here2");
+                wakeUp = true;
+            }
+        }
+
+        if(hourOfDay<0 || hourOfDay>23){
+            System.out.println("Here3");
+            wakeUp=false;
+        }
+
+        return wakeUp;
+
+    }
+
+    private static int calculateHighScore(int score){
+        var position = new AtomicInteger(4);
+
+        if( score >= 1000 ) {
+            position.set(1);
+        } else if( score >= 500 ){
+            position.set(2);
+        } else if( score >= 100 ){
+            position.set(3);
+        }
+
+        return position.get();
+
+    }
+
+    private static void printHighScore(String playerName, int position){
+        System.out.println(playerName + " managed to get into position " + position + " on the high score table");
+    }
+
+    public static void printMegaAndKiloByte(int kiloBytes){
+        int MB = kiloBytes/1024;
+        int KB_remainder = kiloBytes % 1024;
+        System.out.println(kiloBytes + " KB="+MB+" MB and " + KB_remainder + " KB");
+    }
+
 
 }
